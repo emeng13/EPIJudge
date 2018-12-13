abafd1245860d309213cbb34615a74b64d731802
Emily Meng <ewm2136@columbia.edu>
2018-12-12 21:11:38 -0500

    completed hanoi v1

--- epi_judge_cpp/hanoi.cc
+++ epi_judge_cpp/hanoi.cc
@@ -9,10 +9,38 @@ using std::array;
 using std::stack;
 using std::vector;
 const int kNumPegs = 3;
-vector<vector<int>> ComputeTowerHanoi(int num_rings) {
-  // TODO - you fill in here.
-  return {};
+
+void moveTower(int n, array<stack<int>, kNumPegs>& pegs, int src, int dst, int tmp, vector<vector<int>>* resultPtr) {
+  // base case
+  if (n == 0) return;
+
+  // move top n-1 from src to tmp
+  moveTower(n-1, pegs, src, tmp, dst, resultPtr);
+
+  // move nth from src to dst
+  pegs[dst].push(pegs[src].top());
+  pegs[src].pop();
+  resultPtr->emplace_back(vector<int>{src, dst});
+
+  // move top n-1 from tmp to dst
+  moveTower(n-1, pegs, tmp, dst, src, resultPtr);
 }
+
+vector<vector<int>> ComputeTowerHanoi(int num_rings) {
+  // create pegs
+  array<stack<int>, kNumPegs> pegs;
+
+  // push rings onto src
+  for (int i = num_rings; i >= 1; --i) {
+    pegs[0].push(i);
+  }
+
+  // recurse
+  vector<vector<int>> result;
+  moveTower(num_rings, pegs, 0, 1, 2, &result);
+  return result;
+}
+
 void ComputeTowerHanoiWrapper(TimedExecutor& executor, int num_rings) {
   array<stack<int>, kNumPegs> pegs;
   for (int i = num_rings; i >= 1; --i) {
