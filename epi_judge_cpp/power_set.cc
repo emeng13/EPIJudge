a8c779083f9d7cb66bf5bf10ebd78ebc197ab0ff
Emily Meng <ewm2136@columbia.edu>
2018-12-16 18:57:31 -0500

    completed power_set v1

--- epi_judge_cpp/power_set.cc
+++ epi_judge_cpp/power_set.cc
@@ -1,31 +1,51 @@
 #include <algorithm>
 #include <vector>
 #include "test_framework/generic_test.h"
+
 using std::find;
+using std::make_unique;
 using std::vector;
 
-void powerSet(vector<int> set, vector<vector<int>>* resultPtr) {
-  if (set.size() == 0) {
-    return;
-  } 
+// v1 timeout
+// void powerSet(vector<int> set, vector<vector<int>>* resultPtr) {
+//   if (set.size() == 0) {
+//     return;
+//   } 
+//   else {
+//     if (std::find(resultPtr->begin(), resultPtr->end(), set) == resultPtr->end()) {
+//       resultPtr->emplace_back(set);
+//     }
+
+//     for (int i = 0; i < set.size(); i++) {
+//       vector<int> minusSet = set;
+//       minusSet.erase(minusSet.begin() + i);
+
+//       powerSet(minusSet, resultPtr);
+//     }
+//   } 
+// }
+
+void powerSet(vector<int> set, int n, vector<int>* subset, vector<vector<int>>* resultsPtr) {
+  if (set.size() == n) {
+    resultsPtr->emplace_back(*subset);
+  }
   else {
-    if (std::find(resultPtr->begin(), resultPtr->end(), set) == resultPtr->end()) {
-      resultPtr->emplace_back(set);
-    }
+    subset->emplace_back(set.at(n));
 
-    for (int i = 0; i < set.size(); i++) {
-      vector<int> minusSet = set;
-      minusSet.erase(minusSet.begin() + i);
+    powerSet(set, n + 1, subset, resultsPtr);
 
-      powerSet(minusSet, resultPtr);
-    }
-  } 
+    subset->pop_back();
+
+    powerSet(set, n + 1, subset, resultsPtr);
+  }
 }
 
 vector<vector<int>> GeneratePowerSet(const vector<int>& input_set) {
   vector<vector<int>> result;
-  powerSet(input_set, &result);
-  result.emplace_back(vector<int>());
+  powerSet(input_set, 0, make_unique<vector<int>>().get(), &result);
+
+  // v1
+  // result.emplace_back(vector<int>());
   return result;
 }
 
