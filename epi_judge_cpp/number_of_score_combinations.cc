6faf8ad29813e1cf30c0152111d10242a86ce35d
Emily Meng <ewm2136@columbia.edu>
2019-01-30 00:54:33 -0500

    completed number_of_score_combinations v1

--- epi_judge_cpp/number_of_score_combinations.cc
+++ epi_judge_cpp/number_of_score_combinations.cc
@@ -1,11 +1,28 @@
 #include <vector>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::cout;
+using std::endl;
 
 int NumCombinationsForFinalScore(int final_score,
                                  const vector<int>& individual_play_scores) {
-  // TODO - you fill in here.
-  return 0;
+  vector<int> arr;
+
+  for (int x = 0; x <= final_score; x++) {
+      if (x == 0) {
+        arr.push_back(1);
+      } else {
+        arr.push_back(0);
+      }
+  }
+
+  for (int i = 0; i < individual_play_scores.size(); i++) {
+    for (int j = individual_play_scores[i]; j <= final_score; j++) {
+      arr[j] += arr[j - individual_play_scores[i]];  
+    }
+  }
+
+  return arr[final_score];
 }
 
 int main(int argc, char* argv[]) {
