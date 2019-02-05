2dfe7fc7a452320f7d94c3cea1ab759114f59cbe
Emily Meng <ewm2136@columbia.edu>
2019-02-05 02:20:00 -0500

    completed knapsack v1

--- epi_judge_cpp/knapsack.cc
+++ epi_judge_cpp/knapsack.cc
@@ -1,6 +1,8 @@
+#include <algorithm>
 #include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
+using std::max;
 using std::vector;
 
 struct Item {
@@ -8,8 +10,25 @@ struct Item {
 };
 
 int OptimumSubjectToCapacity(const vector<Item>& items, int capacity) {
-  // TODO - you fill in here.
-  return 0;
+  vector<vector<int>> values(items.size() + 1, vector<int>(capacity + 1, 0));
+
+  for (int i = 0; i <= items.size(); i++) {
+    for (int j = 0; j <= capacity; j++) {
+      if (i == 0 || j == 0) {
+        values[i][j] = 0;
+      }
+      else if (items[i-1].weight <= j) {
+        int without = values[i-1][j];
+        int with = values[i-1][j - items[i-1].weight] + items[i-1].value;
+        values[i][j] = max(without, with);
+      }
+      else {
+        values[i][j] = values[i-1][j];
+      }
+    }
+  }
+
+  return values[items.size()][capacity];
 }
 template <>
 struct SerializationTraits<Item> : UserSerTraits<Item, int, int> {};
