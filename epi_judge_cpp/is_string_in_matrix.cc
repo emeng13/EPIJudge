0d1b89b99ea7c76b1ee11f655bae366e2cca985e
Emily Meng <ewm2136@columbia.edu>
2019-02-27 04:30:46 -0500

    wip

--- epi_judge_cpp/is_string_in_matrix.cc
+++ epi_judge_cpp/is_string_in_matrix.cc
@@ -1,10 +1,55 @@
+#include <tuple>
+#include <unordered_map>
 #include <vector>
 #include "test_framework/generic_test.h"
+using std::tuple;
+using std::unordered_map;
 using std::vector;
+
+bool checkPattern(const vector<vector<int>>& grid, int m, int n, const vector<int>& pattern, int idx, unordered_map<vector<int>, int>* notFound, int i , int j) {
+  // nothing to check so pattern exists
+  if (pattern.size() == 0) {
+    return true;
+  }
+
+  // location is out of bounds
+  if (i >= m || i < 0 || j >= n || j < 0) {
+    return false;
+  }
+
+  // previous not found already
+  vector<int> custom;
+  custom.push_back(i);
+  custom.push_back(j);
+  custom.push_back(pattern[idx]);
+
+  if (notFound->find(custom) != notFound->end()) {
+    return false;
+  }
+
+  if (checkPattern(grid, m, n, pattern, idx++, notFound, i - 1, j)) {
+    return true;
+  }
+
+  notFound->emplace(custom, 1);
+  return false;
+}
+
 bool IsPatternContainedInGrid(const vector<vector<int>>& grid,
                               const vector<int>& pattern) {
-  // TODO - you fill in here.
-  return true;
+  unordered_map<vector<int>, int> notFound;
+  int m = grid.size();
+  int n = grid[0].size();
+
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+      if (checkPattern(grid, m, n, pattern, 0, &notFound, i, j)) {
+        return true;
+      }
+    }
+  }
+
+  return false;
 }
 
 int main(int argc, char* argv[]) {
